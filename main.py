2e25dc8fc9c7662064ae5f40147294bbcf45bbb4
Gabriel BuenoAzevedo <bielalgumacoisa@gmail.com>
2020-07-16 10:43:57 -0300

    [upt] Pheromones and elitism

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from matplotlib import pyplot as plt
-from classes.ant import Ant
+from classes.antColonyOptimization import AntColonyOptimization
 import numpy as np
 
 #Open input file
@@ -22,32 +21,5 @@ for line in file:
   cities.append( [ float(line_info[1]), float(line_info[2])] )
 cities = np.array(cities)
 
-cities = np.array([
-  [1,1],[1,2],[2,3],[3,2],[3,1]
-])
-
-#Print graph
-# plt.scatter(cities[:,0], cities[:,1], marker="o", s=10)
-# plt.show()
-
-#Get distance from each city (d(i,j) equals to d(j,i))
-distances = np.zeros((len(cities), len(cities)))
-for i in range(0, len(cities)):
-  for j in range(i+1, len(cities)):
-    distances[i,j] = ( (cities[i][0] - cities[j][0])**2 + (cities[i][1] - cities[j][1])**2 ) ** (1/2)
-    distances[j,i] = distances[i][j]
-
-# print(distances)
-ant = Ant(distances, 1, 1)
-
-pheromones = np.ones((len(cities), len(cities))) * 0.001
-path = ant.run(1, pheromones)
-
-best_run = []
-for node in path:
-  best_run.append(cities[node])
-best_run.append(cities[1])
-
-best_run = np.array(best_run)
-plt.plot(best_run[:,0], best_run[:,1], marker="o", markersize=10)
-plt.show()
+aco = AntColonyOptimization(len(cities), cities, 100, 0.5)
+aco.run(500)
